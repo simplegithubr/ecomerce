09eef894b81b3c8864b5eee6f0477acce843de2c
sagar sheikh <160107305+simplegithubr@users.noreply.github.com>
2025-01-01 09:27:40 -0800

    Add files via upload

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -15,15 +15,15 @@ function page() {
         <div className='flex w-[700px] mx-auto '>
           
           <div className=' '>
-            <h1 className='font-bold text-2xl' >About Kinzaa's Gadgets</h1>
+            <h1 className='font-bold text-2xl' >About Kinzaa s Gadgets</h1>
             <p className='mt-3 '>At Kinzaa s Gadgets we believe in making the latest technology accessible to everyone  Founded by Kinzaa a passionate entrepreneur and artist  our mission is to bring you the best gadgets mobile phones and accessories at unbeatable prices
 
-We take pride in offering:
+We take pride in offering
 
 High-quality products From top brands to trending gadgets
-Customer satisfaction Ensuring a smooth and enjoyable shopping experience.
+Customer satisfaction Ensuring a smooth and enjoyable shopping experience
 Personalized service Because every customer matters to us
-Join us on this journey to explore innovation reliability and style—all under one roof. Thank you for choosing Kinzaa s Gadgets as your trusted tech partner!</p>
+Join us on this journey to explore innovation reliability and style—all under one roof Thank you for choosing Kinzaa s Gadgets as your trusted tech partner</p>
           </div>
           <Image src="/hero.png" alt='' width={300} height={200} ></Image>
         </div>
